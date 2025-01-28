31f7e35a1fa3ea7218fc7f7291b4597627106079
Sstmtz <sxy2367221250@163.com>
2025-01-29 05:23:50 +0800

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,64 @@
+# █▀ █▀▀ █▀█ █ █▀█ ▀█▀ █▀
+# ▄█ █▄▄ █▀▄ █ █▀▀ ░█░ ▄█
+# scripts
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Detect AUR wrapper
+if pacman -Qi yay >/dev/null
+    set aurhelper yay
+else if pacman -Qi paru >/dev/null
+    set aurhelper paru
+end
+
+# ▄▀█ █░░ █ ▄▀█ █▀
+# █▀█ █▄▄ █ █▀█ ▄█
+# alias
+
+# Common
+alias cls='clear' # clear terminal
+alias mkdir='mkdir -p' # create directory
+
+# List shortcuts
+alias l='eza -lh --icons=auto' # long list
+alias ls='eza -1 --icons=auto' # short list
+alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
+alias ld='eza -lhD --icons=auto' # long list dirs
+alias lt='eza --icons=auto --tree' # list folder as tree
+
+# Dirctory navigation shortcuts
+alias ..='cd ..'
+alias ...='cd ../..'
+alias .3='cd ../../..'
+alias .4='cd ../../../..'
+alias .5='cd ../../../../..'
+
+# Package manager shortcuts
+alias un='$aurhelper -Rns' # uninstall package
+alias up='$aurhelper -Syu' # update system/package/aur
+alias pl='$aurhelper -Qs' # list installed package
+alias pa='$aurhelper -Ss' # list available package
+alias pc='$aurhelper -Sc' # remove unused cache
+alias po='$aurhelper -Qtdq | $aurhelper -Rns -' # remove unused packages, also try > $aurhelper -Qqd | $aurhelper -Rsu --print -
+
+# █▀ █▀▀ ▀█▀
+# ▄█ ██▄ ░█░
+# set
+
+# Path related
+set PATH $PATH $HOME/.local/share/bin # add local bin to PATH
+set DOCKER_HOST unix://$XDG_RUNTIME_DIR/docker.sock # set local docker socket
+
+# Universal Variables
+set -U EDITOR nvim # set default text editor
+
+# █▀ █▀█ █░█ █▀█ █▀▀ █▀▀
+# ▄█ █▄█ █▄█ █▀▄ █▄▄ ██▄
+# source
+
+# [starship]
+starship init fish | source
+# [zoxide]
+zoxide init fish | source
---
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,83 @@
+return {
+    {
+        "nvzone/typr",
+        cmd = "TyprStats",
+        dependencies = "nvzone/volt",
+        opts = {},
+    },
+
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open LazyGit floating window" },
+        },
+    },
+
+    {
+        "mgierada/lazydocker.nvim",
+        dependencies = { "akinsho/toggleterm.nvim" },
+        config = function()
+            require("lazydocker").setup({
+                border = "curved", -- valid options are "single" | "double" | "shadow" | "curved"
+            })
+        end,
+        event = "BufRead",
+        keys = {
+            {
+                "<leader>ld",
+                function()
+                    require("lazydocker").open()
+                end,
+                desc = "Open Lazydocker floating window",
+            },
+        },
+    },
+
+    {
+        "mikavilpas/yazi.nvim",
+        event = "VeryLazy",
+        keys = {
+            -- 👇 in this section, choose your own keymappings!
+            {
+                "<leader>-",
+                mode = { "n", "v" },
+                "<cmd>Yazi<cr>",
+                desc = "Open yazi at the current file",
+            },
+            {
+                -- Open in the current working directory
+                "<leader>cw",
+                "<cmd>Yazi cwd<cr>",
+                desc = "Open the file manager in nvim's working directory",
+            },
+            {
+                -- NOTE: this requires a version of yazi that includes
+                -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+                "<c-up>",
+                "<cmd>Yazi toggle<cr>",
+                desc = "Resume the last yazi session",
+            },
+        },
+        opts = {
+            -- if you want to open yazi instead of netrw, see below for more info
+            open_for_directories = false,
+            keymaps = {
+                show_help = "<f1>",
+            },
+        },
+    },
+}
