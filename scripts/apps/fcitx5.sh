30e00b218c0775e2f605f53c098794eef7b25a25
Sstmtz <sxy2367221250@163.com>
2025-02-05 04:04:52 +0800

    scripts: fix synatx issues

--- scripts/apps/fcitx5.sh
+++ scripts/apps/fcitx5.sh
@@ -1,18 +1,38 @@
 #!/bin/env bash
 
-# set input method variables
-echo 'INPUT_METHOD=fcitx' | sudo tee /etc/environment >/dev/null
-# xim for xwayland support
-echo 'XMODIFIERS="@im=fcitx"' | sudo tee /etc/environment >/dev/null
-# for sdl2 support
-echo 'SDL_IM_MODULE=fcitx' | sudo tee /etc/environment >/dev/null
-# for glfw support. eg. kitty
-ehco 'GLFW_IM_MODULE=ibus' | sudo tee /etc/environment >/dev/null
+# 定义要添加的变量
+input_method='INPUT_METHOD=fcitx'
+xmodifiers='XMODIFIERS="@im=fcitx"'  # xim for xwayland support
+sdl_im_module='SDL_IM_MODULE=fcitx'  # for sdl2 support
+glfw_im_module='GLFW_IM_MODULE=ibus' # for glfw support. eg. kitty
+# gtk_im_module='GTK_IM_MODULE=fcitx'  # for gtk support, only for x11
+# qt_im_module='QT_IM_MODULE=fcitx'    # for qt support, only for x11
 
-# for gtk support, only for x11
-# echo 'GTK_IM_MODULE=fcitx' | sudo tee /etc/environment >/dev/null
-# for qt support, only for x11
-# echo 'QT_IM_MODULE=fcitx' | sudo tee /etc/environment >/dev/null
+if ! grep -q "^$input_method" /etc/environment; then
+	echo "$input_method" | sudo tee -a /etc/environment >/dev/null
+fi
+
+if ! grep -q "^$xmodifiers" /etc/environment; then
+	echo "$xmodifiers" | sudo tee -a /etc/environment >/dev/null
+fi
+
+if ! grep -q "^$sdl_im_module" /etc/environment; then
+	echo "$sdl_im_module" | sudo tee -a /etc/environment >/dev/null
+fi
+
+if ! grep -q "^$glfw_im_module" /etc/environment; then
+	echo "$glfw_im_module" | sudo tee -a /etc/environment >/dev/null
+fi
+
+# only for x11
+# if ! grep -q "^$gtk_im_module" /etc/environment; then
+# 	echo "$gtk_im_module" | sudo tee -a /etc/environment >/dev/null
+# fi
+
+# only for x11
+# if ! grep -q "^$qt_im_module" /etc/environment; then
+# 	echo "$qt_im_module" | sudo tee -a /etc/environment >/dev/null
+# fi
 
 # References:
 # https://wiki.archlinux.org/title/Fcitx5#IM_modules
