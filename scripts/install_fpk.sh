c38aef7939db138be39494ddb855eecde515bd8f
Sstmtz <sxy2367221250@163.com>
2025-02-09 07:54:32 +0800

    Update Lists and add install_fpk.sh

---
+++ scripts/install_fpk.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+scrDir=$(dirname "$(realpath "$0")")
+if ! source "${scrDir}/global_fn.sh"; then
+	echo "Error: unable to source global_fn.sh..."
+	exit 1
+fi
+
+listFpk=$(awk -F '#' '{print $1}' "${1}" | sed 's/ //g' | xargs)
+locale=$(locale | grep LANG | cut -d= -f2)
+
+# Check if flatpak is installed
+if ! pkg_installed flatpak; then
+	sudo pacman -S flatpak
+fi
+
+# Add flathub repo url
+if [[ "$locale" == "en_US.UTF-8" ]]; then
+	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+elif [[ "$locale" == "zh_CN.UTF-8" ]]; then
+	flatpak remote-add --if-not-exists flathub https://mirror.sjtu.edu.cn/flathub/flathub.flatpakrepo
+fi
+
+flatpak install flathub ${listFpk}
+flatpak remove --unused
--- scripts/install_pkg.sh
+++ scripts/install_pkg.sh
@@ -17,7 +17,7 @@ while IFS= read -r line; do
 		pkg=$(echo "$line" | awk '{print $1}' | xargs)
 		if [[ -n $pkg ]]; then
 			if pkg_installed "${pkg}"; then
-				print_log -y "[skip] " "${pkg} has installed."
+				print_log -y "[SKIP] " "${pkg} has installed."
 			elif pkg_available "${pkg}"; then
 				if [[ "$locale" == "en_US.UTF-8" ]]; then
 					repo=$(pacman -Si "${pkg}" | awk -F ': ' '/Repository / {print $2}' | head -n 1)
@@ -27,23 +27,23 @@ while IFS= read -r line; do
 					echo "Unsupported locale: $locale"
 				fi
 				archPkg+=("${pkg}")
-				print_log -b "[queue] " -g "${repo}" -b "::" "${pkg}"
+				print_log -b "[QUEUE] " -g "${repo}" -b "::" "${pkg}"
 			elif aur_available "${pkg}"; then
 				aurhPkg+=("${pkg}")
-				print_log -b "[queue] " -g "aur" -b "::" "${pkg}"
+				print_log -b "[QUEUE] " -g "aur" -b "::" "${pkg}"
 			else
-				print_log -r "[error] " "unknown package ${pkg}..."
+				print_log -r "[ERROR] " "Unknown package ${pkg}..."
 			fi
 		fi
 	fi
 done <"${listPkg}"
 
 if [[ ${#archPkg[@]} -gt 0 ]]; then
-	print_log -b "[install] " "arch packages..."
+	print_log -b "[Install] " "arch packages..."
 	sudo pacman -S "${archPkg[@]}"
 fi
 
 if [[ ${#aurhPkg[@]} -gt 0 ]]; then
-	print_log -b "[install] " "aur packages..."
+	print_log -b "[Install] " "aur packages..."
 	"${aurhlpr}" -S "${aurhPkg[@]}"
 fi
