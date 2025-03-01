9ef2fe05ba0d0822d3753afdbf12e7a23eb948cc
Sstmtz <sxy2367221250@163.com>
2025-03-01 18:53:09 +0800

    nvim: adjust

--- home/dot_config/nvim/lua/plugins/integrated.lua
+++ home/dot_config/nvim/lua/plugins/integrated.lua
@@ -41,6 +41,7 @@ return {
         },
     },
     -- integrated with yazi
+    ---@type LazySpec
     {
         "mikavilpas/yazi.nvim",
         event = "VeryLazy",
@@ -59,13 +60,12 @@ return {
                 desc = "Open the file manager in nvim's working directory",
             },
             {
-                -- NOTE: this requires a version of yazi that includes
-                -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
                 "<c-up>",
                 "<cmd>Yazi toggle<cr>",
                 desc = "Resume the last yazi session",
             },
         },
+        ---@type YaziConfig | {}
         opts = {
             -- if you want to open yazi instead of netrw, see below for more info
             open_for_directories = false,
